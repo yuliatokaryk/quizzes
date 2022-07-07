976f49081e7582741e1aaeb937ebd5ad7469e6b5
yuliatokaryk <julia4099@gmail.com>
2022-07-07 13:29:06 +0300

    schema deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_073243) do
-
-  create_table "quizzes", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
