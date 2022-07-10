7a128320846d3703518aa66028da87a8bd68e3a1
yuliatokaryk <julia4099@gmail.com>
2022-07-10 17:49:37 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_073243) do
+ActiveRecord::Schema.define(version: 2022_07_10_143820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quizzes", force: :cascade do |t|
     t.string "title"
